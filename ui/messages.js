22558265bc4a5918b908a942e408c95be6049963
ryanschuhler <ryan.schuhler@liferay.com>
2017-10-11 12:35:48 -0700

    Cookie check

--- ui/messages.js
+++ ui/messages.js
@@ -10,6 +10,7 @@ function checkPassword(event) {
 
 	if (event.target.value == 'password') {
 		passwordCotainer.parentNode.removeChild(passwordCotainer);
+		setCookie("approved", "true", 30);
 
 		getData();
 	}
@@ -17,6 +18,38 @@ function checkPassword(event) {
 		event.preventDefault();
 
 		errorContainer.style.visibility = 'visible';
+	}
+}
+
+function setCookie(cname,cvalue,exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires=" + d.toGMTString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function checkCookie() {
+	var approved = getCookie("approved");
+
+	if (approved != "") {
+		getData();
 	}
 }
 
